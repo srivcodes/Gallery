b68c687ae613fad214194bcd0775200f3e2cf2e1
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2021-07-07 22:06:18 +0530

    hookep up to select the image on click

--- gallery.js
+++ gallery.js
@@ -8,6 +8,29 @@ function Gallery (gallery){
     const prevButton = document.querySelector(".prev");
     const nextButton = document.querySelector(".next");
 
+    function showImage (el){
+        if(!el){
+            console.info("No Image to show");
+            return;
+        }
+        console.log(el);
+    }
+
+    function handleImageClick(e){
+        showImage(e.currentTarget);
+    }
+
+    images.forEach(image => {
+        // attach an event listener for each image
+        image.addEventListener('click', e => {
+          // when that is keyup'd, check if it was enter
+        //   if (e.key === 'Enter') {
+            // if it was, show that image
+            showImage(e.currentTarget);
+        //   }
+        });
+      });
+
 }
 
 const gallery1 = Gallery(document.querySelector(".gallery1"));
